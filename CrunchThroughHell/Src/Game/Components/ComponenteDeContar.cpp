9b281ad0a85c93ba8f35ab8db5f1dbaafc662451
Yojhan <yojhansg@ucm.es>
2022-03-25 00:12:51 +0100

    Cambios timer

--- CrunchThroughHell/Src/Game/Components/ComponenteDeContar.cpp
+++ CrunchThroughHell/Src/Game/Components/ComponenteDeContar.cpp
@@ -2,8 +2,16 @@
 #include <iostream>
 #include <SceneManager.h>
 #include <GameTime.h>
+#include <Timer.h>
+#include <GameObject.h>
+
+void ComponenteDeContar::init() {
+	//Timer::deleteComponent(this, 1);
+	Timer::deleteGameObject(gameObject, 1);
+	//Timer::invoke([&]() {std::cout << (scene == nullptr ? "sii" : "nooo"); }, 1);
+}
 
 void ComponenteDeContar::update() {
-
-	std::cout << "Frame rate: " << 1.0f / LoveEngine::Time::getInstance()->deltaTime << "\n";
+	count++;
+	//std::cout << "Frame rate: " << 1.0f / LoveEngine::Time::getInstance()->deltaTime << "\n";
 }
--- CrunchThroughHell/Src/Game/Components/ComponenteDeContar.h
+++ CrunchThroughHell/Src/Game/Components/ComponenteDeContar.h
@@ -4,5 +4,6 @@
 class ComponenteDeContar : public Component {
 	int count = 0;
 public:
+	void init() override;
 	void update() override;
 };
