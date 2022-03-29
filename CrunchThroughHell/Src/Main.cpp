7074ecdcef5e3584e26c97369e49b382b91aaaf4
Yojhan <yojhansg@ucm.es>
2022-03-29 16:19:06 +0200

    Eliminado Scene Definitions

--- CrunchThroughHell/Src/Main.cpp
+++ CrunchThroughHell/Src/Main.cpp
@@ -1,13 +1,8 @@
 #include <Windows.h>
-#include "SceneDefinition.h"
 #include "ComponentDefinition.h"
 
 extern "C" {
-	__declspec(dllexport) SceneDefinitions* quierounaescena() {
-		auto scenedef = new SceneDefinitions();
-		scenedef->scenesDefinitions();
-		return scenedef;
-	}
+	
 	__declspec(dllexport) void componentDefinition() {
 		LoveEngine::defineComponents();
 	}
