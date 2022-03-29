9ae183b8fdd7ade58cf622a3343ebf0b21104b86
Yojhan <yojhansg@ucm.es>
2022-03-29 23:05:19 +0200

    Refactorización factoria de componentes

--- CrunchThroughHell/Src/ComponentDefinition.cpp
+++ CrunchThroughHell/Src/ComponentDefinition.cpp
@@ -5,33 +5,15 @@
 #include <Components/ComponenteDeContar.h>
 namespace LoveEngine {
 
-	class ContarCreator: public ComponentCreator {
-	public:
-		ContarCreator() : ComponentCreator("ComponenteDeContar"){}
-		Component* CreateComponent() override { return new ComponenteDeContar(); }
-	};
-
-	class AutodestruccionCreator : public ComponentCreator {
-	public:
-		AutodestruccionCreator() : ComponentCreator("Autodestruccion") {}
-		Component* CreateComponent() override { return new Autodestruccion(); }
-	};
-
-	class FrameRateCreator : public ComponentCreator {
-	public:
-		FrameRateCreator() : ComponentCreator("FrameRate") {}
-		Component* CreateComponent() override { return new FrameRate(); }
-	};
-
-
 	void defineComponents() {
 
 		ComponentFactory* inst = ComponentFactory::getInstance();
 		assert(inst != nullptr);
 
-		inst->registerComponent(new ContarCreator());
-		inst->registerComponent(new AutodestruccionCreator());
-		inst->registerComponent(new FrameRateCreator());
+
+		inst->registerComponent(new ComponentCreatorTemplate<ComponenteDeContar>("ComponenteDeContar"));
+		inst->registerComponent(new ComponentCreatorTemplate<Autodestruccion>("AutoDestruccion"));
+		inst->registerComponent(new ComponentCreatorTemplate<FrameRate>("FrameRate"));
 	}
 
 }
