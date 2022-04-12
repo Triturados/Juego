8d600a74f9a3aacfba3290a910715f494490ea8c
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2022-04-12 16:11:47 +0200

    fixed mistake camera rotate

--- CrunchThroughHell/Game/CamRotate.h
+++ CrunchThroughHell/Game/CamRotate.h
@@ -1,38 +1,40 @@
-//#pragma once
-//#include <Component.h>
-//
-//
-//namespace LoveEngine {
-//	namespace Input {
-//		class InputManager;
-//	}
-//
-//	namespace ECS {
-//		class Transform;
-//		class CamRotate : public Component {
-//
-//		private:
-//			Transform* playerTr;
-//			Transform* bossTr;
-//			Transform* camTr;
-//
-//			GameObject* boss;
-//			GameObject* player;
-//
-//			Input::InputManager* input;
-//			float speed = 15;
-//			float rotSpeed = 5;
-//			float horiSens = 1;
-//			float verSens = 1;
-//
-//			bool followBoss;
-//
-//			Utilities::Vector2<float> mousePos;
-//		public:
-//			void init() override;
-//			void update() override;
-//			void receiveMessage(Utilities::StringFormatter& message) override;
-//			void receiveGameObject(int n, GameObject* b) override;
-//		};
-//	}
-//}
+#pragma once
+#include <Component.h>
+
+
+namespace LoveEngine {
+	namespace Input {
+		class InputManager;
+	}
+
+	namespace ECS {
+		class Transform;
+		class CamRotate : public Component {
+
+		private:
+			Transform* playerTr;
+			Transform* bossTr;
+			Transform* camTr;
+
+			GameObject* boss;
+			GameObject* player;
+
+			Input::InputManager* input;
+			float speed = 15;
+			float rotSpeed = 5;
+			float horiSens = 1;
+			float verSens = 1;
+
+			bool followBoss;
+
+			Utilities::Vector2<float>* mousePos;
+		public:
+			~CamRotate();
+
+			void init() override;
+			void update() override;
+			void receiveMessage(Utilities::StringFormatter& message) override;
+			void receiveGameObject(int n, GameObject* b) override;
+		};
+	}
+}
