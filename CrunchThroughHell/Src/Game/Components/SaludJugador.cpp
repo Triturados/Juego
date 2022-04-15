950ebf8b44f33edbde39ced603ad849ebeed9efb
iquintasALT <iquintas@ucm.es>
2022-04-15 05:08:45 +0200

    salud del jugador se modifica y muestra en pantalla correctamente

--- CrunchThroughHell/Src/Game/Components/SaludJugador.cpp
+++ CrunchThroughHell/Src/Game/Components/SaludJugador.cpp
@@ -3,16 +3,38 @@
 #include "Transform.h"
 #include "RigidBody.h"
 #include "Vector3.h"
-#include "Vector4.h"
-#include "GameTime.h"
-#include "Input.h"
+
 #include <StringFormatter.h>
 #include <iostream>
+#include <Slider.h>
+
+
+#include <Input.h>
 
 void LoveEngine::ECS::SaludJugador::receiveMessage(Utilities::StringFormatter& sf)
 {
 	/*sf.tryGetFloat("speed", speed);
 	sf.tryGetFloat("rotSpeed", rotSpeed);*/
+}
+
+void LoveEngine::ECS::SaludJugador::addHealth()
+{
+	actHealth++;
+	if (actHealth > _MAX_HEALTH) actHealth = _MAX_HEALTH;
+}
+
+void LoveEngine::ECS::SaludJugador::setHealth(int health)
+{
+	if (health < 0) health = 0;
+	else if (health > _MAX_HEALTH) health = _MAX_HEALTH;
+	actHealth = health;	
+
+
+}
+
+void LoveEngine::ECS::SaludJugador::addMaxHealth()
+{
+	_MAX_HEALTH++;
 }
 
 void LoveEngine::ECS::SaludJugador::init()
@@ -22,12 +44,21 @@ void LoveEngine::ECS::SaludJugador::init()
 
 	tr = gameObject->getComponent<Transform>();
 	//assert
+	slider = gameObject->getComponent<Slider>();
+
+	actHealth = _MAX_HEALTH;
+	slider->setDetectInput(false);
+	slider->setPos(100, 100);	
 }
 
 void LoveEngine::ECS::SaludJugador::update()
 {
-	
+	if(input->keyJustPressed(Input::InputKeys::A)) {
+		setHealth(getHealth() - 1);
+	}
 
+	int barProgress = actHealth * slider->MAX_VALUE / _MAX_HEALTH;
+	slider->setProgress(barProgress);
 }
 
 
--- CrunchThroughHell/Src/Game/Components/SaludJugador.h
+++ CrunchThroughHell/Src/Game/Components/SaludJugador.h
@@ -10,16 +10,26 @@ namespace LoveEngine {
 	namespace ECS {
 		class Transform;
 		class RigidBody;
+		class Slider;
 		class SaludJugador : public Component {
 			
 		private:
-			Transform* tr;
-			Input::InputManager* input;
+			Transform* tr = nullptr;
+			Slider* slider = nullptr;
+			Input::InputManager* input = nullptr;
+
+			int _MAX_HEALTH = 4;
+			int actHealth = 4;
 
 		public:
 			void init() override;
 			void update() override;
 			void receiveMessage(Utilities::StringFormatter& message) override;
+
+			inline int getHealth() { return actHealth; }
+			void addHealth();
+			void setHealth(int health);
+			void addMaxHealth();
 		};
 	}
 }
