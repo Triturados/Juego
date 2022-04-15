17f7e0ec15cfaa24e748dbb45a0146908b9fb142
edekadt <edekadt@ucm.es>
2022-04-15 02:50:03 +0200

    Añadido acciones del boss

--- CrunchThroughHell/Src/Game/Components/ComportamientoBoss.cpp
+++ CrunchThroughHell/Src/Game/Components/ComportamientoBoss.cpp
@@ -14,9 +14,120 @@ namespace LoveEngine
     {
         void ComportamientoBoss::init()
         {
+            auto meleeAttack = new MeleeAttack(this);
+            addAction(meleeAttack);
+            auto chase = new Chase(this);
+            addAction(chase);
+            auto leap = new Leap(this);
+            addAction(leap);
+            /*
+            auto player = receiveGameObject();
+            meleeAttack->setTarget(player);
+            chase->setTarget(player);
+            leap->setTarget(player);
+            */
             Agent::init();
         }
 
+        ComportamientoBoss::MeleeAttack::MeleeAttack(Agent* agent_) : Action(agent_, 10.0)
+        {
+            rb = agent->gameObject->getComponent<RigidBody>();
+            tr = agent->gameObject->getComponent<Transform>();
+            increasePrioOverTime = 10.0;
+        };
+
+        void ComportamientoBoss::MeleeAttack::setTarget(Transform* t)
+        {
+            target = t;
+        }
+
+        bool ComportamientoBoss::MeleeAttack::conditionsFulfilled() const
+        {
+            if (target == nullptr) return true; // TO DO: Change true to false
+            return (*(target->getPos()) - *(tr->getPos())).magnitude() < 10;
+        }
+
+        void ComportamientoBoss::MeleeAttack::onActionStart()
+        {
+            std::cout << "\n\n\n\n\n\n\nAttacking at melee\n\n\n\n\n\n\n\n";
+            setPriority(30.0);
+            if (target == nullptr || rb == nullptr || tr == nullptr)
+            {
+                //throw new std::exception("Faltan referencias para una acción");
+                return;
+            }
+            // TO DO: start animation
+        }
+
+        void ComportamientoBoss::MeleeAttack::activeUpdate()
+        {
+            // TO DO: lookat target
+        }
+
+
+        ComportamientoBoss::Idle::Idle(Agent* agent_) : Action(agent_, 8.0) {}
+
+        void ComportamientoBoss::Idle::activeUpdate() { std::cout << "Idle\n"; }
+
+        ComportamientoBoss::Chase::Chase(Agent* agent_) : Action(agent_, 0.0) { };
+
+        void ComportamientoBoss::Chase::setTarget(Transform* t)
+        {
+            target = t;
+        }
+
+        void ComportamientoBoss::Chase::activeUpdate()
+        {
+            if ((*(rb->getVelocity())).magnitude() < maxVel)
+            {
+                Utilities::Vector3<float> targetPos = *(target->getPos());
+                Utilities::Vector3<float> pos = *(tr->getPos());
+
+                rb->addForce((targetPos - pos).getNormalized() * acc * rb->getMass(), Utilities::Vector3<float>(0, 0, 0), (int)ForceMode::ACCELERATION);
+                //lookat target
+            }
+        }
+
+        ComportamientoBoss::Leap::Leap(Agent* agent_) : Action(agent_, 80)
+        {
+            rb = agent->gameObject->getComponent<RigidBody>();
+            tr = agent->gameObject->getComponent<Transform>();
+            increasePrioOverTime = 10;
+        }
+
+        void ComportamientoBoss::Leap::setTarget(Transform* t)
+        {
+            target = t;
+        }
+
+        bool ComportamientoBoss::Leap::conditionsFulfilled() const
+        {
+            if (target == nullptr) return true; // TO DO: Change true to false
+            return (*(target->getPos()) - *(tr->getPos())).magnitude() > 40;
+        }
+
+        void ComportamientoBoss::Leap::onActionStart()
+        {
+            std::cout << "\n\n\n\n\n\n\nLeaping\n\n\n\n\n\n\n\n";
+            if (target == nullptr || rb == nullptr || tr == nullptr)
+            {
+                //throw new std::exception("Faltan referencias para una acción");
+                return;
+            }
+
+            //start animation
+            rb->addForce(*(target->getPos()) - *(tr->getPos()) + Utilities::Vector3<float>(0, 10, 0), Utilities::Vector3<float>(0, 0, 0), (int)ForceMode::IMPULSE);
+        }
+
+        void ComportamientoBoss::Leap::activeUpdate()
+        {
+            setPriority(80);
+            if (target == nullptr || rb == nullptr || tr == nullptr) return;
+            // TO DO: remove these lines ^^^^
+            if (rb->getVelocity()->y > 0 && rb->getVelocity()->y < 0.02)
+                setPriority(80);
+            //end animation
+        }
     }
 }
 
--- CrunchThroughHell/Src/Game/Components/ComportamientoBoss.h
+++ CrunchThroughHell/Src/Game/Components/ComportamientoBoss.h
@@ -8,6 +8,56 @@ namespace LoveEngine {
 		class Transform;
 		class RigidBody;
 		class ComportamientoBoss : public Agent {
+		protected:
+#pragma region Actions
+			class MeleeAttack : public Action
+			{
+			public:
+				MeleeAttack(Agent* agent_);
+				void setTarget(Transform* t);
+				bool conditionsFulfilled() const final;
+				void onActionStart() final;
+				void activeUpdate() final;
+			protected:
+				RigidBody* rb;
+				Transform* target = nullptr;
+				Transform* tr = nullptr;
+			};
+            class Idle : public Action
+            {
+            public:
+                Idle(Agent* agent_);
+                void activeUpdate() final;
+            };
+
+            class Chase : public Action
+            {
+            public:
+                Chase(Agent* agent_);
+                void setTarget(Transform* t);
+                void activeUpdate() final;
+            protected:
+                float maxVel = 10;
+                float acc = 10;
+                RigidBody* rb = nullptr;
+                Transform* target = nullptr;
+                Transform* tr = nullptr;
+            };
+
+            class Leap : public Action
+            {
+            public:
+                Leap(Agent* agent_);
+                void setTarget(Transform* t);
+                bool conditionsFulfilled() const final;
+                void onActionStart() final;
+                void activeUpdate() final;
+            protected:
+                RigidBody* rb;
+                Transform* target = nullptr;
+                Transform* tr = nullptr;
+            };
+#pragma endregion
 		private:
 			Transform* target;
 
