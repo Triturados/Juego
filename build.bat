b39d7fde54349b07cd7623d65ce4de30ec5e9ba5
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2022-04-27 17:20:25 +0200

    Creado un ejecutable build
    Es el único .bat que el usuario debe ejecutar

---
+++ build.bat
@@ -0,0 +1,19 @@
+echo off
+
+title Realizando la build - Crunch Through Hell
+echo Comenzando con la magia!
+
+rem Variables de batch
+set WORKING_DIR=%cd%
+set ENGINE_DIR=.\CrunchThroughHell\Engine\
+
+cd %ENGINE_DIR%
+
+call automate_reset.bat
+
+call automate.bat
+
+cd %WORKING_DIR%    
+
+pause
+exit
