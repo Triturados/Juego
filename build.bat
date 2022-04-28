060fa885081c5e92bc6bee3f051f530a17cf34df
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2022-04-29 00:05:41 +0200

    El .bat compila el juego

--- build.bat
+++ build.bat
@@ -6,8 +6,10 @@ echo Comenzando con la magia!
 rem Variables de batch
 set WORKING_DIR=%cd%
 set ENGINE_DIR=.\CrunchThroughHell\Engine\
+set CRUNCH_THROUGH_HELL_SOL=.\CrunchThroughHell\CrunchThroughHell.sln
 
-git submodule update --remote
+rem Inicializamos el submódulo
+git submodule update --init
 
 cd %ENGINE_DIR%
 
@@ -15,7 +17,13 @@ call automate_reset.bat
 
 call automate.bat
 
-cd %WORKING_DIR%    
+cd %WORKING_DIR%
+
+rem Compilacion de la solucion del juego
+msbuild %CRUNCH_THROUGH_HELL_SOL% /p:configuration=Debug /p:Platform=x64
+msbuild %CRUNCH_THROUGH_HELL_SOL% /p:configuration=Release /p:Platform=x64
+
+echo Magia hecha, a jugar!
 
 pause
 exit
