203d3dbc90ed288c4c4491d562db0d4f6209f432
Cristian Castillo de Leon <62943663+Crisca06@users.noreply.github.com>
2022-04-28 20:04:05 +0200

    Avance en los proyectiles

--- CrunchThroughHell/Src/Game/Components/Bullet.cpp
+++ CrunchThroughHell/Src/Game/Components/Bullet.cpp
@@ -2,23 +2,40 @@
 #include "Timer.h"
 #include <iostream>
 #include "GameObject.h"
-#include "RigidBody.h"
+#include "Transform.h"
+#include "StringFormatter.h"
 #include "Vector3.h"
+#include "SaludJugador.h"
+#include "Mesh.h"
 
 void LoveEngine::ECS::Bullet::init()
 {
 	lifetime = Timer::deleteGameObject(gameObject, 10);
-	if (gameObject->getComponent<RigidBody>()) mRb = gameObject->getComponent<RigidBody>();
-	vel = new Utilities::Vector3<float>( 0, 10, 0 );
+	tr = gameObject->getComponent<Transform>();
+	dir->normalize();
+	mesh = gameObject->getComponent<Mesh>();
 }
 
 void LoveEngine::ECS::Bullet::update()
 {
 	std::cout << "Este componente se autodestruira en: " << lifetime->timeLeft() << "\n";
 
-	if (gameObject->isEnabled()) {
-		if (mRb != nullptr) {
-			mRb->addForce(*vel, Utilities::Vector3<float>(0, 0, 0), ForceMode::FORCE);
-		}
-	}
+	if (gameObject->isEnabled())
+		tr->translate(*dir * vel);
+}
+
+void LoveEngine::ECS::Bullet::receiveMessage(Utilities::StringFormatter& sf)
+{
+	sf.tryGetVector3("direction", *dir);
+	sf.tryGetFloat("velocity", vel);
+	sf.tryGetInt("damage", damage);
+}
+
+void LoveEngine::ECS::Bullet::enterCollision(GameObject* other)
+{
+	if (!other->getComponent<SaludJugador>()) return;
+
+	other->getComponent<SaludJugador>()->takeDamage(damage);
+	mesh->setVisibility(false);
+	gameObject->removeGameObject();
 }
--- CrunchThroughHell/Src/Game/Components/Bullet.h
+++ CrunchThroughHell/Src/Game/Components/Bullet.h
@@ -7,15 +7,22 @@ namespace LoveEngine {
 	namespace ECS {
 		class Timer;
 		class RigidBody;
+		class Transform;
+		class Mesh;
 		class Bullet : public Component {
 			
 		private:
 			Timer* lifetime = nullptr;
-			RigidBody* mRb = nullptr;
-			Utilities::Vector3<float>* vel;
+			Transform* tr;
+			Mesh* mesh;
+			Utilities::Vector3<float>* dir;
+			float vel;
+			int damage;
 		public:
 			void init() override;
 			void update() override;
+			void receiveMessage(Utilities::StringFormatter& sf) override;
+			void enterCollision(GameObject* other) override;
 		};
 	}
 }
