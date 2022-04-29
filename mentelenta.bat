6f68110cdcc5ea08a4156533ad5ff2f21b74b16b
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2022-04-29 17:31:50 +0200

    Si te cuesta pensar, este commit es para ti

---
+++ mentelenta.bat
@@ -0,0 +1,26 @@
+@echo off
+
+title Te faltan dos veranos - Crunch Through Hell
+echo Comenzando con lo que cualquiera haría pero a ti te lo tiene que hacer un ejecutable de windows!
+
+rem Variables de batch
+set WORKING_DIR=%cd%
+set ENGINE_DIR=.\CrunchThroughHell\Engine\
+set SKELETON_SOL=.\CrunchThroughHell\Engine\Skeleton\Skeleton.sln
+set CRUNCH_THROUGH_HELL_SOL=.\CrunchThroughHell\CrunchThroughHell.sln
+
+rem Inicializamos el submódulo
+git submodule update --remote
+
+rem Compilacion de la solucion del motor
+msbuild %SKELETON_SOL% /p:configuration=Debug /p:Platform=x64
+msbuild %SKELETON_SOL% /p:configuration=Release /p:Platform=x64
+
+rem Compilacion de la solucion del juego
+msbuild %CRUNCH_THROUGH_HELL_SOL% /p:configuration=Debug /p:Platform=x64
+msbuild %CRUNCH_THROUGH_HELL_SOL% /p:configuration=Release /p:Platform=x64
+
+echo Magia hecha, a jugar!
+
+pause
+exit
