fcd18cc392bc9e875f7d7c0697173f829f45ba2a
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2022-05-03 00:59:03 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -11,9 +11,6 @@ set CRUNCH_THROUGH_HELL_SOL=.\CrunchThroughHell\CrunchThroughHell.sln
 rem Inicializamos el submódulo
 git submodule init
 
-rem Cambiar a main cuando se haga el merge de Functional a main
-git config -f .gitmodules submodule.CrunchThroughHell/Engine.branch Functional
-
 git submodule update
 
 rem Se aniade el Ogre.log que se ignora
