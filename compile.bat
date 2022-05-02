d293d83f4e3c096768c5fa8b3a7a9e1fed0d62fb
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2022-05-03 00:57:51 +0200

    Compile.bat ya no actualiza el motor

--- compile.bat
+++ compile.bat
@@ -9,16 +9,6 @@ set SKELETON_SOL=.\CrunchThroughHell\Engine\Skeleton\Skeleton.sln
 set CRUNCH_THROUGH_HELL_SOL=.\CrunchThroughHell\CrunchThroughHell.sln
 set ENGINE_DIR=.\CrunchThroughHell\Engine\
 
-rem Actualizamos el submodulo
-git submodule update --remote --merge
-
-cd %ENGINE_DIR%
-
-git checkout Functional
-git pull
-
-cd %GAME_ROOT_DIR%
-
 rem Compilacion de la solucion del motor
 msbuild %SKELETON_SOL% /p:configuration=Debug /p:Platform=x64 /p:PlatformToolset=v142
 msbuild %SKELETON_SOL% /p:configuration=Release /p:Platform=x64 /p:PlatformToolset=v142
