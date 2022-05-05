e1370b2f9b97447b4902cacc98328b5c2c030046
Yojhan <yojhansg@ucm.es>
2022-05-05 17:07:18 +0200

    inicios shop

---
+++ CrunchThroughHell/Src/Game/Components/Shop.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <Component.h>
+
+
+namespace LoveEngine {
+
+	namespace ECS {
+
+		class Button;
+		class Shop : public Component {
+
+		public:
+
+			Shop();
+			~Shop();
+			void receiveComponent(int, Component*) override;
+			static bool SAVEDATA;
+
+		private:
+			const int maxPurchase = 3;
+			const char* path = "shop";
+
+			int currency;
+			int healt, damage, speed;
+
+			void buyHealth();
+			void buyDamage();
+			void buySpeed();
+		};
+	}
+}
